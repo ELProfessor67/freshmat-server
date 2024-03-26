ab00f61fe2b288231475df8de0501baa5ba9384d
elprofessor67 <jeeshanr599@gmail.com>
2024-03-26 08:37:04 -0700

    first commit

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -2,8 +2,9 @@ export const sendToken = (res, user, message, statusCode = 200) => {
     const token = user.getJWTToken();
     const options = {
       expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-      secure: false,
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
       // sameSite: "none",
     };
   
